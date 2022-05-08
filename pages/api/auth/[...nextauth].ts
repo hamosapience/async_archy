2662fb663a09d8241213daf1b0f9b91773418c2e
Valentin Kamenek <valentin.kamenek@holidu.com>
2022-05-08 14:32:34 +0200

    connect to Auth0

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,20 +1,22 @@
 import NextAuth from "next-auth";
 
-import GithubProvider from "next-auth/providers/github";
+import Auth0Provider from "next-auth/providers/auth0";
 
 export default NextAuth({
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID!,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET!,
+      issuer: process.env.AUTH0_ISSUER,
     }),
   ],
   theme: {
     colorScheme: "light",
   },
   callbacks: {
-    async jwt({ token }) {
-      token.userRole = "admin";
+    async jwt({ token, profile, user, account }) {
+      console.log("token", token, "p", profile, "u", user, "a", account);
+      token.roles = profile?.["http://localhost:3000/roles"];
       return token;
     },
   },
---
+++ pages/taskList/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useSession } from "next-auth/react";
+import AccessDenied from "pages/AccessDenied";
+
+const TaskList = () => {
+  const { data: session, status } = useSession();
+
+  console.log(session, status);
+
+  // When rendering client side don't display anything until loading is complete
+  if (status === "loading") return null;
+
+  if (!session) {
+    return <AccessDenied />;
+  }
+
+  return <div>TASK LIST</div>;
+};
+
+export default TaskList;
